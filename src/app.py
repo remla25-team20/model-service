209b469bd4aee7fb3389cad8d99a666334599404
Adrian Josan <a.josan@student.tudelft.nl>
2025-05-06 21:38:46 +0200

    Run service on 8080

--- src/app.py
+++ src/app.py
@@ -32,4 +32,4 @@ def predict():
     return jsonify(prediction=prediction)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=106, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
